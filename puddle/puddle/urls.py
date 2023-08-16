8c61797b24b40b709c7174000259c310c7ed911e
pritamgurung97 <124842594+pritamgurung97@users.noreply.github.com>
2023-08-16 02:25:11 +0100

    initial commit

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('dashboard/',include('dashboard.urls')),
     path('inbox/',include('conversation.urls')),
     path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
